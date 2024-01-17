cbc9f11a29a29b853d43382a9f44734e34d6b58e
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-01-17 11:35:06 -0300

    created shopping list, using *ngFor and bootstrap classes to style

--- app-project/src/app/shopping-list/shopping-list.component.ts
+++ app-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ShoppingListComponent {
  
-   ingredients=[]
+   ingredients: Ingredient[]=[
+    new Ingredient("apples",5 ),
+    new Ingredient("Tomatos",3)
+   ]
 
 
 }
