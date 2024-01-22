b1ea99f6f5239640879914e110fc175fb82f99ee
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-01-22 11:53:36 -0300

    changing style dynamic passing the color in the tag via Input annotation

--- app-project/src/app/directives/best-highlight.directive.ts
+++ app-project/src/app/directives/best-highlight.directive.ts
@@ -1,27 +1,32 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBestHighlight]'
 })
 export class BestHighlightDirective implements OnInit {
    
-  @HostBinding("style.backgroundColor") backgrounColor :string;
+  @Input( ) defaultColor:string="transparent"
+
+  @Input( ) highlightColor:string="blue"
+
+  @HostBinding("style.backgroundColor") backgrounColor :string=this.defaultColor;
   
   constructor(private elRef: ElementRef, private render: Renderer2) { }
 
 
   ngOnInit(): void {
     //   this.render.setStyle(this.elRef.nativeElement, "color ", "green")
+    this.backgrounColor=this.defaultColor
   }
 
   @HostListener("mouseenter") mouseover(eventData: Event) {
     // this.render.setStyle(this.elRef.nativeElement, "background-color", "green")
-    this.backgrounColor="green"
+    this.backgrounColor=this.highlightColor
   }
 
   @HostListener("mouseleave") mouseleave(eventData: Event) {
     // this.render.setStyle(this.elRef.nativeElement, "background-color", "transparent")
-    this.backgrounColor="transparent"
+    this.backgrounColor=this.defaultColor
   }
 
 }
