a234ee0a5590d1c379ffa7535a1601f3ee82f392
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-01-22 14:14:33 -0300

    implementig dropDown dynamic function using directives

--- app-project/src/app/shared/drop-down.directive.ts
+++ app-project/src/app/shared/drop-down.directive.ts
@@ -1,9 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appDropDown]'
 })
 export class DropDownDirective {
+
+  @HostBinding("class.open") isOpen: boolean = false;
+
+
+
+  @HostListener("click") onClick(eventData: Event) {
+    this.isOpen = !this.isOpen
+
+  }
 
   constructor() { }
 
