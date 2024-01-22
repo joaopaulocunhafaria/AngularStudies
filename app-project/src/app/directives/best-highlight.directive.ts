8b7557d1f0975ef406e4b940f67cefa568106d03
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-01-22 11:43:19 -0300

    using HostBiding to make  little changes on the style of an element using the directives

--- app-project/src/app/directives/best-highlight.directive.ts
+++ app-project/src/app/directives/best-highlight.directive.ts
@@ -1,22 +1,27 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBestHighlight]'
 })
 export class BestHighlightDirective implements OnInit {
-
+   
+  @HostBinding("style.backgroundColor") backgrounColor :string;
+  
   constructor(private elRef: ElementRef, private render: Renderer2) { }
+
 
   ngOnInit(): void {
     //   this.render.setStyle(this.elRef.nativeElement, "color ", "green")
   }
 
   @HostListener("mouseenter") mouseover(eventData: Event) {
-    this.render.setStyle(this.elRef.nativeElement, "background-color", "green")
+    // this.render.setStyle(this.elRef.nativeElement, "background-color", "green")
+    this.backgrounColor="green"
   }
 
   @HostListener("mouseleave") mouseleave(eventData: Event) {
-    this.render.setStyle(this.elRef.nativeElement, "background-color", "transparent")
+    // this.render.setStyle(this.elRef.nativeElement, "background-color", "transparent")
+    this.backgrounColor="transparent"
   }
 
 }
