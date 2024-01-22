5689eddffc886ea00a95abbb36c8b217169ce14b
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-01-22 11:37:31 -0300

    using hostListener to change some styles in a fluid way

--- app-project/src/app/directives/best-highlight.directive.ts
+++ app-project/src/app/directives/best-highlight.directive.ts
@@ -1,14 +1,22 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBestHighlight]'
 })
-export class BestHighlightDirective implements OnInit{
+export class BestHighlightDirective implements OnInit {
 
-  constructor(private elRef:ElementRef,private render:Renderer2) { }
+  constructor(private elRef: ElementRef, private render: Renderer2) { }
 
   ngOnInit(): void {
-    this.render.setStyle(this.elRef.nativeElement, "color ", "green")
+    //   this.render.setStyle(this.elRef.nativeElement, "color ", "green")
+  }
+
+  @HostListener("mouseenter") mouseover(eventData: Event) {
+    this.render.setStyle(this.elRef.nativeElement, "background-color", "green")
+  }
+
+  @HostListener("mouseleave") mouseleave(eventData: Event) {
+    this.render.setStyle(this.elRef.nativeElement, "background-color", "transparent")
   }
 
 }
