657aba6de2f8fdd836310ad1cde3372d088bf7cb
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-01-25 10:06:19 -0300

    destroing subscription when destroy component to no use memory

--- routing-start/routing-start/src/app/users/user/user.component.ts
+++ routing-start/routing-start/src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number, name: string };
-
+   private subscription:Subscription
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -17,14 +18,16 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params["id"]
     }
 
-    this.route.params
+    this.subscription=this.route.params
       .subscribe(
         (params: Params) => {
           this.user.name = params["name"];
           this.user.id = params["id"];
         }
       )
-
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()    
+  }
 }
