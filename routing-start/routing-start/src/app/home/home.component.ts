9f16b1b891905011078213f7d1f55f9762cf1458
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-01-29 13:29:41 -0300

    using routes, acitvedLink, fragments and querParamns

--- routing-start/routing-start/src/app/home/home.component.ts
+++ routing-start/routing-start/src/app/home/home.component.ts
@@ -14,6 +14,6 @@ export class HomeComponent implements OnInit {
   }
 
   loadEditServer(id : number){
-    this.route.navigate(['servers',id,'edit'], {queryParams:{allowEdit:5}, fragment:"loading"})
+    this.route.navigate(['servers',id,'edit'], {queryParams:{allowEdit:4}, fragment:"loading"})
   }
 }
