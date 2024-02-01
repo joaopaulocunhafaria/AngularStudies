48af01cd74d84f2d2d2f365150008eea3f6e5ee3
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-02-01 09:56:47 -0300

    enable the edit button on recipe detail, adding the edit and new recipe routes

--- app-project/src/app/app-routing.module.ts
+++ app-project/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { NgModule } from "@angular/core";
 import { RecipeDetaiComponent } from "./recipes/recipe-detai/recipe-detai.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
-
+//importante ficar atento a ordem das rotas, pra saber
+//quais tem prioridade
 const appRoutes:Routes=[
   {path:"",component:RecipesComponent},
   {path:"recipes",component:RecipesComponent, children:[
     {path:"", component:RecipeStartComponent},
-    {path:":name", component:RecipeDetaiComponent}
+    {path:"new", component:RecipeEditComponent},
+    {path:":name", component:RecipeDetaiComponent},
+    {path:":name/edit", component:RecipeEditComponent},
   ]},
   {path:"shopping-list", component: ShoppingListComponent}
  ]
--- app-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ app-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrl: './recipe-edit.component.css'
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit {
+
+  name: string;
+  editMode: boolean = false;
+
+  constructor(private router: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.router.params.subscribe(
+      (params: Params) => {
+        this.name = params["name"];
+        this.editMode = params["name"] != null;
+      }
+    )
+  }
+
+
 
 }
