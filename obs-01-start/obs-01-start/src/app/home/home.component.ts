918991b56299901425ce4dbb821561e899dd069f
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-02-01 11:20:46 -0300

    creating an custom   observable and triger data using the methodo observer.next

--- obs-01-start/obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/obs-01-start/src/app/home/home.component.ts
@@ -1,19 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { combineAll } from 'rxjs-compat/operator/combineAll';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObservableSubscription: Subscription;
 
   constructor() { }
 
+
   ngOnInit() {
-    interval( 1000).subscribe(count=>{
-      console.log(count)
+    // this.firstObservableSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+
+    const personalizedIntervalObserver = Observable.create(observer => {
+      let count: number = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000)
+
     })
+
+     this.firstObservableSubscription= personalizedIntervalObserver.subscribe(data=>{
+      console.log(data)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.firstObservableSubscription.unsubscribe();
+    
   }
 
 }
