d26952b74cb5a3cba448b8590045d450e529213e
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-02-08 15:21:18 -0300

    added a formGroup to represent the edit form

--- app-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ app-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../../services/recipe/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,19 +12,34 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
 
   name: string;
+  recipe:Recipe = new  Recipe('','','',[]);
   editMode: boolean = false;
+  recipeForm: FormGroup;
 
-  constructor(private router: ActivatedRoute) { }
+  constructor(private router: ActivatedRoute, private recipeService:RecipeService) { }
 
   ngOnInit(): void {
     this.router.params.subscribe(
       (params: Params) => {
         this.name = params["name"];
         this.editMode = params["name"] != null;
+        this.initForm();
       }
     )
   }
 
+ private initForm(){
+       
+   if (this.editMode) {
+      this.recipe = this.recipeService.finbByName(this.name)
+   }
 
+  this.recipeForm = new FormGroup({
+    'name': new FormControl(this.recipe.name,Validators.required),
+    'imagePath': new FormControl(this.recipe.imagePath,[Validators.required]),
+    'description': new FormControl (this.recipe.description,Validators.required),
+    'ingredients': new   FormArray([])
+  })
+ }
 
 }
--- app-project/src/app/recipes/recipe.model.ts
+++ app-project/src/app/recipes/recipe.model.ts
@@ -1,6 +1,6 @@
 import { Ingredient } from "../shared/ingredient.model";
 
-export class Recipe {
+export class    Recipe {
 
     public name: string;    
     public description: string;
